34ef21746f3f095071efbac48bf048906a226f20
fateehhh <siyaapppp@gmail.com>
2024-03-24 22:16:17 +0700

    Pertanyaan1_Soal nomor 1

--- src/minggu5/Faktorial.java
+++ src/minggu5/Faktorial.java
@@ -2,18 +2,22 @@ package minggu5;
 
 public class Faktorial {
     public int nilai;
-    int faktorialBF(int n){
+
+    int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             fakto *= i;
+            i++;
         }
         return fakto;
     }
-    int faktorialDC (int n){
-        if (n==1) {
+
+    int faktorialDC(int n) {
+        if (n == 1) {
             return 1;
         } else {
-            int fakto = n * faktorialDC(n-1);
+            int fakto = n * faktorialDC(n - 1);
             return fakto;
         }
     }
